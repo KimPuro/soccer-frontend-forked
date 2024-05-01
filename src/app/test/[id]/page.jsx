2ae0a5aa28a5c2ef854866db2ab583d230d2957c
KimPuro <puro@protonmail.ch>
2024-05-01 10:31:06 +0900

    feat : add chart js"

--- src/app/test/[id]/page.jsx
+++
@@ -1,7 +0,0 @@
-import SoccerTestResult from "@/components/results/SoccerTestResult";
-
-export default function SoccerTestPage({params}){
-    return(
-        <SoccerTestResult params={params}/>
-    )
-}
